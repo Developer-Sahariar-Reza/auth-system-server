5d67a438d2068fbd20455fbe2e7641ee1d6f0994
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-09-19 20:44:59 +0600

    news.json added

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const port = 5000;
 
 app.use(cors());
 
+const categories = require("./data/categories.json");
+
 app.get("/", (req, res) => {
   res.send("The News Bengal: Your Source, Your Voice!");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
